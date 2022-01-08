63bc6820a11794ff69b2d68f0588e71df014d143
Richard Allen <rsaxvc@gmail.com>
2022-01-08 10:49:15 -0600

    initial commit

---
+++ analyzer.py
@@ -0,0 +1,104 @@
+from apachelogs import LogParser
+import apachelogs
+from timeit import default_timer as timer
+import traceback
+import os
+import sys
+import sqlite3
+
+has_geoip = False
+try:
+	import geoip2.database
+	has_geoip = True
+except:
+	pass
+
+reader = geoip2.database.Reader('GeoLite2-City.mmdb')
+geoip_cache = dict()
+conn = sqlite3.connect("logdb.db")
+c = conn.cursor()
+if conn is not None:
+    c.execute(""" CREATE TABLE IF NOT EXISTS log_entries (
+					apachelog_bytes_sent INTEGER,
+					apachelog_final_status INTEGER,
+					apachelog_headers_referer VARCHAR,
+					apachelog_headers_useragent VARCHAR,
+					apachelog_remote_host VARCHAR,
+					apachelog_remote_logname VARCHAR,
+					apachelog_remote_user VARCHAR,
+					apachelog_request_line VARCHAR,
+					apachelog_request_time VARCHAR,
+					apachelog_line INTEGER,
+					geoip_city VARCHAR,
+					geoip_country VARCHAR,
+					geoip_subdivision_1 VARCHAR,
+					geoip_subdivision_2 VARCHAR,
+					UNIQUE(apachelog_line,apachelog_remote_host,apachelog_request_line)
+                    ); """)
+
+def do_geoip( remote_host ):
+	location = (None,None,None,None)
+	if remote_host in geoip_cache:
+		return geoip_cache[remote_host]
+	else:
+		try:
+			lookup = reader.city(remote_host)
+			location = ( str(lookup.country.name), str(lookup.subdivision_1.name), str(lookup.subdivision_2.name), str(lookup.city.name) )
+			geoip_cache[remote_host] = location
+			return location
+		except:
+			location = (None,None,None,None)
+			geoip_cache[remote_host] = location
+			return location
+
+def autoparse( filename ):
+	logFormats=[
+		"%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"", #Combined
+		"%h %l %u %t \"%r\" %>s %b" #common
+		]
+	with open(filename) as fp:
+		for lf in logFormats:
+			try:
+				entries=[]
+				line_no = 0
+				for log_entry in LogParser(lf).parse_lines(fp):
+					line_no += 1
+					entry = dict()
+					entry["apachelog_line"] = line_no
+
+					for k in vars(log_entry).keys():
+						if not k.startswith('_') and k != "entry" and k != "format" and k != 'directives':
+							entry["apachelog_"+k] = getattr(log_entry, k)
+
+					if "apachelog_request_time_fields" in entry:
+						del entry["apachelog_request_time_fields"]
+
+					if "apachelog_request_time" in entry:
+						entry["apachelog_request_time"] = entry["apachelog_request_time"].isoformat()
+						
+					if "apachelog_headers_in" in entry:
+						entry["apachelog_headers_referer"] = entry["apachelog_headers_in"]["Referer"]
+						entry["apachelog_headers_useragent"] = entry["apachelog_headers_in"]["User-Agent"]
+						del entry["apachelog_headers_in"]
+
+					(entry['geoip_country'], entry['geoip_subdivision_1'], entry['geoip_subdivision_2'], entry['geoip_city'] ) = do_geoip( entry["apachelog_remote_host"] )
+
+					q = "INSERT INTO log_entries"
+					q += " (" + ",".join(entry.keys()) + ")"
+					q += " VALUES (" + ",".join('?' * len(entry.keys())) + ")"
+					q += " ON CONFLICT DO NOTHING"
+					v = entry.values()
+					#print(q,"<=",v)
+					c.execute(q, list(v))
+				return line_no
+			except apachelogs.errors.InvalidEntryError:
+				continue
+		return 0
+	
+start = timer()
+count = 0
+for filename in os.scandir('logs'):
+	count += autoparse( filename )
+end = timer()
+print("Processed:", count, "records in", str(end-start), "seconds")
+conn.commit()
